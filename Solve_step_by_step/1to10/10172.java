4be30cbd217716be895ac0dbdd6f75ac8f1069a9
MINYOUNG KIM <74680251+min150507@users.noreply.github.com>
2021-07-23 02:23:13 +0900

    Create 10172.java

---
+++ Solve_step_by_step/1to10/10172.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+			
+    System.out.println("|\\_/|\n" + 
+	                "|q p|   /}\n" + 
+        					"( 0 )\"\"\"\\\n" + 
+                  "|\"^\"`    |\n" + /*' -> ` 헷갈리지 않기,,*/
+	                "||_/=\\\\__|");
+	            
+		}
+		
+}
